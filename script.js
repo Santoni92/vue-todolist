501720afc4d21fefdf6eac49d74d090ee38064f4
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-28 18:01:49 +0200

    aggiunta funzione al click del button per aggiungere la nuova task(cosa da fare) all'array toDoThings

--- script.js
+++ script.js
@@ -22,7 +22,9 @@ const app = new Vue({
                 done: true
             }
         ],
-        deletedToDoThings : []    //array dei numeri che rappresentano gli indici dei task già fatti
+        deletedToDoThings : [],   //array dei numeri che rappresentano gli indici dei task già fatti
+        newTasks: [] ,   //array delle nuove task (cose da fare) aggiunte dall'utente mediante tag input
+        newTask: '' //stringa di input inserita dall'utente
     },
      
 
@@ -32,9 +34,19 @@ const app = new Vue({
                 return 'barrato';
             }
         },
+
+
         deleteToDoThing(index){
             this.deletedToDoThings.push(index);  /*se l'elemento viene cliccato passo alla funzione l'indice (la posizione)
                                                  dell'oggetto thing nell'array toDoThings*/
+        },
+
+        addTask(){
+            const newTask = this.newTask.trim();  //elimino eventuali spazi aggiunti dall'utente prima e dopo alla stringa in input; notare che con this.newTask mi sto riferendo alla stringa presente in data
+            if(newTask.lenght > 0){ //verifico che l'utente non abbia inserito una stringa vuota
+                this.newTasks.push(newTask);
+                this.newTask = '';  //azzero il campo di input altrimenti rimarrebbe l'ultima task digitata dall'utente
+            }
         }
     }
 });
