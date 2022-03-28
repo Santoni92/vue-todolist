9428c5a6da81252bc1474a2bb48708b9bb02e1f4
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-28 15:27:04 +0200

    aggiunto div a cui agganciare vue;dentro l'oggetto vue creata la struttura dati delle cose da fare ed utilizzato v-for per creare la lista html

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('VUE JS OK!');
+
+const app = new Vue({
+    el:'#todo',
+    data:{
+        toDoThings : [  //array di oggetti che rappresentano le cose (i task) da fare e se quella determinata cosa è stata fatta oppure no
+            {
+                text:'giocare',
+                done:true
+
+            },
+            {
+                text: 'studiare',
+                done:false
+            },
+            {
+                text: 'portare a passeggio il cane',
+                done : false
+            },
+            {
+                text:'lavare i piatti',
+                done:true
+            }
+        ]
+    }
+});
